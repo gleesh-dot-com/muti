59c7b978b1d0762b17c121a15a47aebe927d0e7c
Blair Replogle <UpBra@users.noreply.github.com>
2025-04-12 09:19:30 -0600

    Initial Commit

---
+++ muti/mutiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mutiApp.swift
+//  muti
+//
+//  Created by blair on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct mutiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
